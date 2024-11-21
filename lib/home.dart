f21dca47eb73bc49ec71b2e1b7b1d2f2d726d12f
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2024-11-20 18:04:52 -0700

    splash img & background color

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xffFFCF06),
+      ),
+    );
   }
 }
